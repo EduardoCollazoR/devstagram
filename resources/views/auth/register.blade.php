05479ea88750f32a369dda4d672a82eb0e184283
EduardoCollazoR <eduardo.collazo16@tectijuana.edu.mx>
2022-05-22 20:33:01 -0700

    first commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+
+
+@section('titulo')
+Registrate
+@endsection
+
+
+@section('contenido')
+
+<div class="md:flex md:justify-center">
+
+    <div class="md:w-4/12 gb-white p-6 rounded-lg shadow-xl">
+        <p>
+
+        </p>
+    </div>
+
+    <div class="md:w-4/12">
+
+        <form>
+            <div class="mb-5">
+                <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">Nombre
+
+                </label>
+                <input id="name" name="name" type="text" placeholder="Nombre" class="border p-3 w-full rounded-lg">
+            </div>
+
+            <div class="mb-5">
+                <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">Nombre Usuario
+
+                </label>
+                <input id="username" name="username" type="text" placeholder="Nombre Usuario" class="border p-3 w-full rounded-lg">
+            </div>
+
+            <div class="mb-5">
+                <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">Correo Electronico
+
+                </label>
+                <input id="email" name="email" type="email" placeholder="Correo Electronico" class="border p-3 w-full rounded-lg">
+            </div>
+
+            <div class="mb-5">
+                <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">Contraseña
+
+                </label>
+                <input id="password" name="password" type="password" placeholder="Contraseña" class="border p-3 w-full rounded-lg">
+            </div>
+
+            <div class="mb-5">
+                <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">Repetir Contraseña
+
+                </label>
+                <input id="password_confirmation" name="password_confirmation" type="password" placeholder="Repetir Contraseña" class="border p-3 w-full rounded-lg">
+            </div>
+
+            <input type="submit" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg" value="Crear Cuenta">
+        </form>
+
+    </div>
+
+</div>
+
+@endsection
